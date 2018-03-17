49b99e86410a6775e474587d96d1d9e5c8b5c0fa
tianyizheng <tianyizheng93@gmail.com>
2018-03-17 12:47:40 -0400

    Initial Commit

---
+++ Faceology/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Faceology
+//
+//  Created by Tianyi Zheng on 3/17/18.
+//  Copyright © 2018 Tianyi Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
